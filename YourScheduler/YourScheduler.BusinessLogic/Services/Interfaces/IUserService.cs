203bde29d43607c21f66bc36b5d1d330c5c03b25
Mariusz Pudlik <marpudlik@wp.pl>
2023-09-18 18:40:34 +0200

    Created CQRS

--- YourScheduler/YourScheduler.BusinessLogic/Services/Interfaces/IUserService.cs
+++ YourScheduler/YourScheduler.BusinessLogic/Services/Interfaces/IUserService.cs
@@ -9,12 +9,12 @@ namespace YourScheduler.BusinessLogic.Services.Interfaces
 {
     public interface IUserService
     {
-        List<ApplicationUserDto> GetAllUsers();
+      //  List<ApplicationUserDto> GetAllUsers();
 
-        ApplicationUserDto GetUserByEmail(string email);
+        //ApplicationUserDto GetUserByEmail(string email);
 
-        void AddUser(ApplicationUserDto user);
-        ApplicationUserDto GetUserById(int id);
+       // void AddUser(ApplicationUserDto user);
+      //  ApplicationUserDto GetUserById(int id);
         void UpdateUser(ApplicationUserDto userDtoUpdated);
     }
 }
---
+++ YourScheduler/YourScheduler.BusinessLogic/YourScheduler/Commands/CreateTeam/CreateTeamCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YourScheduler.BusinessLogic.Models.DTOs;
+
+namespace YourScheduler.BusinessLogic.YourScheduler.Commands.CreateCarWorkshop
+{
+   public class CreateTeamCommand:TeamDto,IRequest
+    {
+    }
+}
