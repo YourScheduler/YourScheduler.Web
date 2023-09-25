6203b1e45e25ec2fd2088ea67793112fdd297541
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-09-25 19:30:09 +0200

    Created AddTeamRole Command and Handler.

---
+++ YourScheduler/YourScheduler.BusinessLogic/Commands/AddTeamRole/AddTeamRoleCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using YourScheduler.Infrastructure.Entities;
+
+namespace YourScheduler.BusinessLogic.Commands.AddTeamRole
+{
+    public class AddTeamRoleQuery : IRequest<TeamRole>
+    {
+        public TeamRole TeamRole { get; set; }
+
+        public AddTeamRoleQuery(TeamRole teamRole)
+        {
+            TeamRole = teamRole;
+        }
+    }
+}
---
+++ YourScheduler/YourScheduler.BusinessLogic/Commands/AddTeamRole/AddTeamRoleCommanderHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using YourScheduler.Infrastructure.Entities;
+using YourScheduler.Infrastructure.Repositories;
+
+namespace YourScheduler.BusinessLogic.Commands.AddTeamRole
+{
+    public class AddTeamRoleQueryHandler : IRequestHandler<AddTeamRoleQuery, TeamRole>
+    {
+        private readonly TeamRoleRepository _teamRoleRepository;
+
+        public AddTeamRoleQueryHandler(TeamRoleRepository teamRoleRepository)
+        {
+            _teamRoleRepository = teamRoleRepository;
+        }
+
+        public async Task<TeamRole> Handle(AddTeamRoleQuery request, CancellationToken cancellationToken)
+        {
+            return await _teamRoleRepository.AddTeamRoleAsync(request.TeamRole);
+        }
+    }
+}
