0a6c96efa10ddf5c5158dacf00b7de021625709a
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-09-27 00:16:44 +0200

    Changed UpdateTeamRoleCommand for TeamRoleDto

--- YourScheduler/YourScheduler.BusinessLogic/Commands/UpdateTeamRole/UpdateTeamRoleCommand.cs
+++ YourScheduler/YourScheduler.BusinessLogic/Commands/UpdateTeamRole/UpdateTeamRoleCommand.cs
@@ -1,9 +1,10 @@
 ﻿using MediatR;
+using YourScheduler.BusinessLogic.Models.DTOs;
 using YourScheduler.Infrastructure.Entities;
 
 namespace YourScheduler.BusinessLogic.Commands.UpdateTeamRole
 {
-    public class UpdateTeamRoleCommand : IRequest<TeamRole>
+    public class UpdateTeamRoleCommand : IRequest<TeamRoleDto>
     {
         public TeamRole TeamRole { get; }
 
--- YourScheduler/YourScheduler.BusinessLogic/Commands/UpdateTeamRole/UpdateTeamRoleCommandHandler.cs
+++ YourScheduler/YourScheduler.BusinessLogic/Commands/UpdateTeamRole/UpdateTeamRoleCommandHandler.cs
@@ -1,22 +1,26 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using MediatR;
+using YourScheduler.BusinessLogic.Models.DTOs;
 using YourScheduler.Infrastructure.Entities;
-using YourScheduler.Infrastructure.Repositories;
 using YourScheduler.Infrastructure.Repositories.Interfaces;
 
 namespace YourScheduler.BusinessLogic.Commands.UpdateTeamRole
 {
-    public class UpdateTeamRoleCommandHandler : IRequestHandler<UpdateTeamRoleCommand, TeamRole>
+    public class UpdateTeamRoleCommandHandler : IRequestHandler<UpdateTeamRoleCommand, TeamRoleDto>
     {
         private readonly ITeamRoleRepository _teamRoleRepository;
+        private readonly IMapper _mapper;
 
-        public UpdateTeamRoleCommandHandler(ITeamRoleRepository teamRoleRepository)
+        public UpdateTeamRoleCommandHandler(ITeamRoleRepository teamRoleRepository, IMapper mapper)
         {
             _teamRoleRepository = teamRoleRepository;
+            _mapper = mapper;
         }
 
-        public async Task<TeamRole> Handle(UpdateTeamRoleCommand request, CancellationToken cancellationToken)
+        public async Task<TeamRoleDto> Handle(UpdateTeamRoleCommand request, CancellationToken cancellationToken)
         {
-            return await _teamRoleRepository.UpdateTeamRoleAsync(request.TeamRole);
+            var returnedTeamRole = await _teamRoleRepository.UpdateTeamRoleAsync(request.TeamRole);
+            return _mapper.Map<TeamRoleDto>(returnedTeamRole);
         }
     }
 }
