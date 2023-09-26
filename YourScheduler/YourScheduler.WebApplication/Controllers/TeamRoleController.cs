89abbcfadc21d1b26eaceaa3f05cfef43c7ecef4
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-09-26 21:18:45 +0200

    Created Endoint for AddTeamRoleQuery

--- YourScheduler/YourScheduler.WebApplication/Controllers/TeamRoleController.cs
+++ YourScheduler/YourScheduler.WebApplication/Controllers/TeamRoleController.cs
@@ -1,8 +1,10 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using YourScheduler.BusinessLogic.Commands.AddTeamRole;
 using YourScheduler.BusinessLogic.Queries.GetAllTeamRolesForTeam;
 using YourScheduler.BusinessLogic.Queries.GetTeamRoleById;
+using YourScheduler.Infrastructure.Entities;
 
 namespace YourScheduler.WebApplication.Controllers
 {
@@ -23,6 +25,7 @@ namespace YourScheduler.WebApplication.Controllers
         public async Task<IActionResult> GetAllTeamRolesForTeam([FromBody] int teamId)
         {
             var returnedTeamRoles = await _mediator.Send(new GetAllTeamRolesForTeamQuery(teamId));
+
             return Ok(returnedTeamRoles);
         }
 
@@ -32,7 +35,18 @@ namespace YourScheduler.WebApplication.Controllers
         public async Task<IActionResult> GetTeamRoleById([FromBody] int teamRoleId)
         {
             var returnedRole = await _mediator.Send(new GetTeamRoleByIdQuery(teamRoleId));
+
             return Ok(returnedRole);
+        }
+
+        [HttpPut]
+        [Authorize]
+        [Route("CreateTeamRole")]
+        public async Task<IActionResult> CreateTeamRole([FromBody] TeamRole teamRole)
+        {
+            var createdTeamRole = await _mediator.Send(new AddTeamRoleQuery(teamRole));
+
+            return new ObjectResult(createdTeamRole) { StatusCode = StatusCodes.Status201Created };
         }
 
 
