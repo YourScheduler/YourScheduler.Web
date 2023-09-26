0a8fe40a080b8dddbf6b55879334f12bef288fc3
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-09-26 21:02:07 +0200

    Created Endpoint for GetAllTeamRolesForTeam

---
+++ YourScheduler/YourScheduler.WebApplication/Controllers/TeamRoleController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using YourScheduler.BusinessLogic.Queries.GetAllTeamRolesForTeam;
+
+namespace YourScheduler.WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamRoleController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TeamRoleController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [Authorize]
+        [Route("getAllTeamRolesForTeam/{input}")]
+        public async Task<IActionResult> GetAllTeamRolesForTeam([FromBody]int teamId)
+        {
+            var returnedTeamRoles = await _mediator.Send(new GetAllTeamRolesForTeamQuery(teamId));
+            return Ok(returnedTeamRoles);
+        }
+
+
+    }
+}
