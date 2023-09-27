16628dbdabde6bc43b64ae6a3723e1d86c31184d
Mariusz Pudlik <marpudlik@wp.pl>
2023-09-27 15:16:57 +0200

    Added validators for  Update Team Role, Create Team, Add User

---
+++ YourScheduler/YourScheduler.BusinessLogic/Commands/AddUser/AddUserCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YourScheduler.Infrastructure.Repositories.Interfaces;
+
+namespace YourScheduler.BusinessLogic.Commands.AddUser
+{
+    public class AddUserCommandValidator:AbstractValidator<AddUserCommand>
+    {
+        public AddUserCommandValidator(IUsersRepository usersRepository)
+        {
+            RuleFor(c => c.UserDto.Name).NotEmpty().MinimumLength(2).WithMessage("Name should have at least 2 characters")
+             .MinimumLength(20).WithMessage("Name should have maximum of 20 characters");
+            RuleFor(c => c.UserDto.Email).EmailAddress().WithMessage("Please enter email");
+        }
+    }
+}
---
+++ YourScheduler/YourScheduler.BusinessLogic/Commands/CreateTeam/CreateTeamCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YourScheduler.BusinessLogic.Models.DTOs;
+using YourScheduler.Infrastructure.Repositories.Interfaces;
+
+namespace YourScheduler.BusinessLogic.Commands.CreateTeam
+{
+    public class CreateTeamCommandValidator:AbstractValidator<CreateTeamCommand>
+    {
+        public CreateTeamCommandValidator(ITeamsRepository teamsRepository)
+        {
+            RuleFor(c => c.TeamDto.Name).NotEmpty().MinimumLength(2).WithMessage("Name should have at least 2 characters")
+                .MinimumLength(20).WithMessage("Name should have maximum of 20 characters");
+            RuleFor(c => c.TeamDto.Description).NotEmpty().WithMessage("Please enter description");
+            
+        }
+    }
+}
