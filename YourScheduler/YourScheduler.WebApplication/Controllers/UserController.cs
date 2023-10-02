480bedf972e35288195d8fff927528c4831559ba
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-10-02 22:54:31 +0200

    Created endpoint for AddUserCommand

--- YourScheduler/YourScheduler.WebApplication/Controllers/UserController.cs
+++ YourScheduler/YourScheduler.WebApplication/Controllers/UserController.cs
@@ -1,84 +1,96 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using YourScheduler.BusinessLogic.Commands.AddUser;
 using YourScheduler.BusinessLogic.Models.DTOs;
-using YourScheduler.BusinessLogic.Services.Interfaces;
+using YourScheduler.Infrastructure.Repositories;
 
 namespace YourScheduler.UI.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IMediator _mediator;
 
-       // private readonly Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> _signInManager;
-        public UserController()
+        public UserController(IMediator mediator)
         {
-                   
+            _mediator = mediator;
         }
-       // GET: UserController
 
-       //[Authorize]
-       // public ActionResult Index()
-       // {
-       //     var userId = int.Parse(HttpContext.User.Identity.GetUserId());
+        [HttpPost]
+        [Authorize]
+        [Route("AddUser")]
+        public async Task<ApplicationUserDto> AddUser([FromBody]ApplicationUserDto applicationUserDto)
+        {
+            return await _mediator.Send(new AddUserCommand(applicationUserDto));
+        }
+
+        // GET: UserController
+
+        //[Authorize]
+        // public ActionResult Index()
+        // {
+        //     var userId = int.Parse(HttpContext.User.Identity.GetUserId());
 
 
-       //     var model = _userService.GetUserById(userId);
-       //     return View(model);
-       // }
+        //     var model = _userService.GetUserById(userId);
+        //     return View(model);
+        // }
 
-       // GET: UserController/Details/5
-       // [Route("details/{id:int}")]
-       // public ActionResult Details()
-       // {
-       //     return RedirectToAction("Create", "Event");
-       // }
+        // GET: UserController/Details/5
+        // [Route("details/{id:int}")]
+        // public ActionResult Details()
+        // {
+        //     return RedirectToAction("Create", "Event");
+        // }
 
-       // GET: UserController/Create
-       // public ActionResult Create()
-       // {
-       //     return RedirectToAction("Create", "Team");
-       // }
+        // GET: UserController/Create
+        // public ActionResult Create()
+        // {
+        //     return RedirectToAction("Create", "Team");
+        // }
 
-       // POST: UserController/Create
-       //[HttpPost]
-       //[ValidateAntiForgeryToken]
-       // public ActionResult Create(IFormCollection collection)
-       // {
-       //     try
-       //     {
-       //         return RedirectToAction(nameof(Index));
-       //     }
-       //     catch
-       //     {
-       //         return View();
-       //     }
-       // }
+        // POST: UserController/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        // public ActionResult Create(IFormCollection collection)
+        // {
+        //     try
+        //     {
+        //         return RedirectToAction(nameof(Index));
+        //     }
+        //     catch
+        //     {
+        //         return View();
+        //     }
+        // }
 
-       // GET: UserController/Edit/5
-       // [Route("edit/{id:int}")]
-       // public ActionResult Edit(int id)
-       // {
-       //     var model = _userService.GetUserById(id);
-       //     return View(model);
-       // }
+        // GET: UserController/Edit/5
+        // [Route("edit/{id:int}")]
+        // public ActionResult Edit(int id)
+        // {
+        //     var model = _userService.GetUserById(id);
+        //     return View(model);
+        // }
 
-       // POST: UserController/Edit/5
-       // [HttpPost]
-       // [ValidateAntiForgeryToken]
+        // POST: UserController/Edit/5
+        // [HttpPost]
+        // [ValidateAntiForgeryToken]
 
-       // [Route("edit/{id:int}")]
-       // public ActionResult Edit(int id, ApplicationUserDto userDto)
-       // {
-       //     try
-       //     {
-       //         _userService.UpdateUser(userDto);
-       //         return RedirectToAction(nameof(Index));
-       //     }
-       //     catch
-       //     {
-       //         return View("Error of editing user");
-       //     }
-       // }
+        // [Route("edit/{id:int}")]
+        // public ActionResult Edit(int id, ApplicationUserDto userDto)
+        // {
+        //     try
+        //     {
+        //         _userService.UpdateUser(userDto);
+        //         return RedirectToAction(nameof(Index));
+        //     }
+        //     catch
+        //     {
+        //         return View("Error of editing user");
+        //     }
+        // }
 
     }
 }
