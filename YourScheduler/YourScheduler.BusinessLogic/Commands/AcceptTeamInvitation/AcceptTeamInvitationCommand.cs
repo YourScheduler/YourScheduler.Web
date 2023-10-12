290ee0e071bb51be1caed50e2ba1d7b38d237387
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-10-12 18:38:54 +0200

    Created CQRS for AcceptTeamInvitationCommand and Handler

---
+++ YourScheduler/YourScheduler.BusinessLogic/Commands/AcceptTeamInvitation/AcceptTeamInvitationCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace YourScheduler.BusinessLogic.Commands.AcceptTeamInvitation
+{
+    public class AcceptTeamInvitationCommand : IRequest
+    {
+        public string Token { get;}
+
+        public AcceptTeamInvitationCommand(string token)
+        {
+            Token = token;
+        }
+    }
+}
