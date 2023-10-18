2f702584454843e0ad32280c77a82c86b916a142
Kamil Majewski <kamil.majewski.t@gmail.com>
2023-10-19 01:03:48 +0200

    Fixed small typo

--- YourScheduler/YourScheduler.BusinessLogic/Commands/AcceptTeamInvitation/AcceptTeamInvitationCommandHandler.cs
+++ YourScheduler/YourScheduler.BusinessLogic/Commands/AcceptTeamInvitation/AcceptTeamInvitationCommandHandler.cs
@@ -42,7 +42,7 @@ namespace YourScheduler.BusinessLogic.Commands.AcceptTeamInvitation
 
 
             await _teamMemberRepository.AddTeamMemberAsUserAsync(int.Parse(userId), int.Parse(TeamId));
-            return "User accepted invite or was approved by the administrator and had his role was changed to user";
+            return "User accepted invite or was approved by the administrator and had his role changed to user";
         }
     }
 }
